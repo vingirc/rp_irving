e6bec66323193854c172932c84cdcc2a1ffc58d9
vingirc <2023371010@uteq.edu.mx>
2026-02-19 19:11:25 -0600

    Botón primeng

---
+++ src/app/button-demo/button-demo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+    selector: 'button-demo',
+    templateUrl: './button-demo.html',
+    imports: [ButtonModule]
+})
+export class ButtonDemo { }
